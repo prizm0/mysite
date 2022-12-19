90f88ead6c239ea66777f940de9b7823c6f5b831
prizm0 <121036163+prizm0@users.noreply.github.com>
2022-12-19 18:56:42 +0500

    first big project

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const scrollUp = document.querySelector('#scroll-up');
+const burger = document.querySelector('#burger-menu');
+const nav = document.querySelector('nav');
+const ul = document.querySelector('nav ul');
+const navLink = document.querySelectorAll('.nav-link')
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+})
+burger.addEventListener("click", () => {
+    ul.classList.toggle('show')
+})
+navLink.forEach((link) => link.addEventListener("click", () => { ul.classList.remove("show"); }));
